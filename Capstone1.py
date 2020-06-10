a41099f180efd7444199ce8586cd3818241b3783
charymcordoba <54373076+charymcordoba@users.noreply.github.com>
2020-06-10 18:06:06 +0200

    Update Capstone1.py

--- Capstone1.py
+++ Capstone1.py
@@ -3,20 +3,14 @@
 
 # This notebook is created for the capstone project
 
-# In[1]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[2]:
-
-
 print("Hello Capstone Project Course!")
 
-
-# In[ ]:
 
 
 
